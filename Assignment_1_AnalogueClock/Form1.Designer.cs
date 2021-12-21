2dc96832c1752101f0114d55462209c0468858f6
rshad <rshad@DESKTOP-SVV690T>
2021-12-21 20:28:06 +0300

    Add project files.

---
+++ Assignment_1_AnalogueClock/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Assignment_1_AnalogueClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.btnSetHoursMinutes = new System.Windows.Forms.Button();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCalculatedAngle = new System.Windows.Forms.TextBox();
+            this.lblCalculatedAngle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(514, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(153, 345);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(114, 20);
+            this.txtHours.TabIndex = 1;
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(339, 345);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(114, 20);
+            this.txtMinutes.TabIndex = 2;
+            // 
+            // btnSetHoursMinutes
+            // 
+            this.btnSetHoursMinutes.Location = new System.Drawing.Point(480, 345);
+            this.btnSetHoursMinutes.Name = "btnSetHoursMinutes";
+            this.btnSetHoursMinutes.Size = new System.Drawing.Size(146, 23);
+            this.btnSetHoursMinutes.TabIndex = 3;
+            this.btnSetHoursMinutes.Text = "Calculate Angle";
+            this.btnSetHoursMinutes.UseVisualStyleBackColor = true;
+            this.btnSetHoursMinutes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Location = new System.Drawing.Point(109, 345);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(38, 13);
+            this.lblHours.TabIndex = 4;
+            this.lblHours.Text = "Hours:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Minutes:";
+            // 
+            // txtCalculatedAngle
+            // 
+            this.txtCalculatedAngle.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCalculatedAngle.Enabled = false;
+            this.txtCalculatedAngle.Location = new System.Drawing.Point(204, 385);
+            this.txtCalculatedAngle.Name = "txtCalculatedAngle";
+            this.txtCalculatedAngle.Size = new System.Drawing.Size(422, 20);
+            this.txtCalculatedAngle.TabIndex = 6;
+            // 
+            // lblCalculatedAngle
+            // 
+            this.lblCalculatedAngle.AutoSize = true;
+            this.lblCalculatedAngle.Location = new System.Drawing.Point(111, 385);
+            this.lblCalculatedAngle.Name = "lblCalculatedAngle";
+            this.lblCalculatedAngle.Size = new System.Drawing.Size(87, 13);
+            this.lblCalculatedAngle.TabIndex = 7;
+            this.lblCalculatedAngle.Text = "Calculated Angle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCalculatedAngle);
+            this.Controls.Add(this.txtCalculatedAngle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.btnSetHoursMinutes);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "AssignmentAnalogueClock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Button btnSetHoursMinutes;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCalculatedAngle;
+        private System.Windows.Forms.Label lblCalculatedAngle;
+    }
+}
+
---
+++ Assignment_1_AnalogueClock/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1_AnalogueClock
+{
+    public partial class Form1 : Form
+    {
+        int WIDTH = 300, HEIGHT = 300, secHAND = 140, minHAND = 110, hrHAND = 80;
+
+        //center
+        int cx, cy;
+
+        Bitmap bmp;
+        Graphics g;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int txtHoursInput;
+            int txtMinutesInput;
+
+            if (validateHoursMinutes())
+            {
+                txtHoursInput = Convert.ToInt32(txtHours.Text);
+                txtMinutesInput = Convert.ToInt32(txtMinutes.Text);
+                setHoursMinute(txtHoursInput, txtMinutesInput);
+                txtCalculatedAngle.Text = calcAngle(txtHoursInput, txtMinutesInput).ToString();
+            }
+        }
+
+        public Boolean validateHoursMinutes()
+        {
+            Boolean ret = true;
+            int parsedValue;
+
+            if (txtHours.Text == "")
+            {
+                MessageBox.Show("Hours must be filled in.");
+                ret = false;
+            }
+
+            if (txtMinutes.Text == "")
+            {
+                MessageBox.Show("Minutes must be filled in.");
+                ret = false;
+            }
+
+            if (!int.TryParse(txtHours.Text, out parsedValue))
+            {
+                MessageBox.Show("Hours must be a number.");
+                ret = false;
+            }
+
+            if (!int.TryParse(txtMinutes.Text, out parsedValue))
+            {
+                MessageBox.Show("Minutes must be a number.");
+                ret = false;
+            }
+
+            if (txtHours.Text != "" && int.TryParse(txtHours.Text, out parsedValue))
+            {
+                if (Convert.ToInt32(txtHours.Text) > 12 || Convert.ToInt32(txtHours.Text) <= 0)
+                {
+                    MessageBox.Show("Hours must be between 1 and 12.");
+                    ret = false;
+                }
+            }
+
+            if (txtMinutes.Text != "" && int.TryParse(txtMinutes.Text, out parsedValue))
+            {
+                if (Convert.ToInt32(txtMinutes.Text) > 60 || Convert.ToInt32(txtMinutes.Text) < 0)
+                {
+                    MessageBox.Show("Hours must be between 1 and 12.");
+                    ret = false;
+                }
+            }
+
+            return ret;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create bitmap
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+
+            //center
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+
+            //backcolor
+            this.BackColor = Color.White;
+
+            setHoursMinute(12, 00);
+        }
+
+        public void setHoursMinute(int Hours, int Minute)
+        {
+            //create graphics
+            g = Graphics.FromImage(bmp);
+
+            int[] handCoord = new int[2];
+
+            //clear
+            g.Clear(Color.White);
+
+            //draw circle
+            g.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, WIDTH, HEIGHT);
+
+            //draw figure
+            g.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(140, 2));
+            g.DrawString("3", new Font("Arial", 12), Brushes.Black, new PointF(286, 140));
+            g.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(142, 282));
+            g.DrawString("9", new Font("Arial", 12), Brushes.Black, new PointF(0, 140));
+
+            //minute hand
+            handCoord = msCoord(Minute, minHAND);
+            g.DrawLine(new Pen(Color.Black, 2f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //hour hand
+            handCoord = hrCoord(Hours % 12, Minute, hrHAND);
+            g.DrawLine(new Pen(Color.Gray, 3f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //load bmp in picturebox1
+            pictureBox1.Image = bmp;
+
+            //dispose
+            g.Dispose();
+        }
+
+        //coord for minute and second hand
+        private int[] msCoord(int val, int hlen)
+        {
+            int[] coord = new int[2];
+            val *= 6;   //each minute and second make 6 degree
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        //coord for hour hand
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+
+            //each hour makes 30 degree
+            //each min makes 0.5 degree
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        static double calcAngle(double h, double m)
+        {
+            double hourInDegrees = (h * 30) + (m * 30.0 / 60);
+            double minuteInDegrees = m * 6;
+            double diff = Math.Abs(hourInDegrees - minuteInDegrees);
+            if (diff > 180)
+            {
+                diff = 360 - diff;
+            }
+
+            return diff;
+        }
+    }
+}
